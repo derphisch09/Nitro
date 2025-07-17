631818707f3b239063e9a6957e702d704b2c4293
jjj <jrnw1706@gmail.com>
2025-07-17 16:03:06 +0200

    Update CPlayerResource.h

--- Amalgam/src/SDK/Definitions/Main/CPlayerResource.h
+++ Amalgam/src/SDK/Definitions/Main/CPlayerResource.h
@@ -19,4 +19,12 @@ public:
 	NETVAR_ARRAY(m_iAccountID, unsigned, "CPlayerResource", "m_iAccountID");
 	NETVAR_ARRAY(m_bValid, bool, "CPlayerResource", "m_bValid");
 	NETVAR_ARRAY(m_iUserID, int, "CPlayerResource", "m_iUserID");
+
+	inline void SetAlive(int index, bool value)
+	{
+		if (index < 0 || index > 101)
+			return;
+
+		*reinterpret_cast<bool*>(reinterpret_cast<uintptr_t>(this) + m_bAlive(index) + sizeof(bool) * index) = value;
+	};
 };
