41121f4bfe99d7f7f4217777db3a44e9b3de7253
jjj <jrnw1706@gmail.com>
2025-07-17 16:02:12 +0200

    Update CBaseCombatCharacter.cpp

--- Amalgam/src/SDK/Definitions/Main/CBaseCombatCharacter.cpp
+++ Amalgam/src/SDK/Definitions/Main/CBaseCombatCharacter.cpp
@@ -12,5 +12,22 @@ CTFWeaponBase* CBaseCombatCharacter::GetWeaponFromSlot(int nSlot)
 {
 	if (nSlot < 0 || nSlot >= MAX_WEAPONS)
 		return nullptr;
+
 	return m_hMyWeapons()[nSlot].Get();
+}
+
+CTFWeaponBase* CBaseCombatCharacter::FindWeaponByItemDefinitionIndex(int definition_index)
+{
+	for (int n; n < MAX_WEAPONS; n++)
+	{
+		CTFWeaponBase* weapon = GetWeaponFromSlot(n);
+
+		if (!weapon)
+			continue;
+
+		if (weapon->m_iItemDefinitionIndex() == definition_index)
+			return weapon;
+	}
+
+	return nullptr;
 }
